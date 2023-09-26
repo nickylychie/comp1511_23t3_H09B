7f183c6a8b34789e27a6871e3026da9a44d02202
Nicole <z5206727@unsw.edu.au>
2023-09-26 15:42:24 +1000

    Tutorial demo code

---
+++ week2/program.c
@@ -0,0 +1,18 @@
+// Nicole, 23T3 Week 2
+// Tutorial demo code
+
+#include <stdio.h>
+
+int main(void) {
+    // scan in numbers into a and b
+    
+
+    // if a < b, print error, finish program
+
+    // if b equals 0, print different error, finish program
+
+    // if a > b, print the stuff
+
+    return 0;
+}
+
