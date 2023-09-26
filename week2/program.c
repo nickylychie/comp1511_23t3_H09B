ab672425ad83169b3ea04bdaf1334c97e0cfb3df
Nicole <z5206727@unsw.edu.au>
2023-09-26 15:45:35 +1000

    Tutorial demo code

--- week2/program.c
+++ week2/program.c
@@ -4,14 +4,25 @@
 #include <stdio.h>
 
 int main(void) {
-    // scan in numbers into a and b
     
+    // scan in 2 values, a and b
+    int a;
+    int b;
+    printf("Please enter 2 numbers: ");
+    scanf("%d %d", &a, &b);
 
-    // if a < b, print error, finish program
-
-    // if b equals 0, print different error, finish program
-
-    // if a > b, print the stuff
+    // if a < b, print error message - "ERROR"
+    if (a < b) {
+        printf("ROAR!!");
+    }
+    // else if b == 0, print another error message
+    else if (b == 0) {
+        printf("meow~");
+    }
+    // else if a > b, print a / b and (a * 1.0) / (b * 1.0)
+    else if (a > b) {
+        printf("%d, %lf\n", a/b, (a * 1.0) / (b * 1.0));
+    }
 
     return 0;
 }
