2b52ec64330df097ffc91b4c7614ed9a87251caf
Nicole <z5206727@unsw.edu.au>
2023-10-24 16:25:53 +1100

    Tutorial code ready

---
+++ week7/command_line_arguments.c
@@ -0,0 +1,14 @@
+// Nicole, 23T3 Week 7
+// Tutorial demo: Command Line Arguments
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+
+    // print total number of command line arguments
+
+    // print each command line argument on a new line 
+    // with the index of the line
+
+    return 0;
+}
